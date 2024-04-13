522cdc341dd5b75034cd559db257e6df496923f9
Minkyu Choi <minkyu.choi07@gmail.com>
2024-04-12 23:44:32 -0500

    fix lint

--- example.py
+++ example.py
@@ -1,6 +1,9 @@
-def main():
+"""Example main run script for the project."""
+
+
+def main() -> None:
     """Provide example code to run your code / application."""
-    print("You're all set")
+    print("You're all set")  # noqa: T201
 
 
 if __name__ == "__main__":
